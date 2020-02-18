1177be63bacf05195cc57cbdce98ee2d5061d463
cycy2323 <809461180@qq.com>
2020-02-18 14:18:09 +0800

    vue-ts

---
+++ uikit/src/Home.vue
@@ -0,0 +1,133 @@
+<template>
+    <div class="root-home">
+        <div class="btn-container">
+            <UIButton @click="clickBtn"
+                      :xsmall="xsmall"
+                      :small="small"
+                      :large="large"
+                      :xlarge="xlarge"
+                      :tile="tile"
+                      :rounded="rounded"
+                      :circle="circle"
+                      :disabled="disabled"
+            >H</UIButton>
+        </div>
+        <div class="btn-group">
+            <UIButton class="btn" @click="resize('xsmall')">超小</UIButton>
+            <UIButton class="btn" @click="resize('small')" >小</UIButton>
+            <UIButton class="btn" @click="resize('normal')" >正常</UIButton>
+            <UIButton class="btn" @click="resize('large')" >大</UIButton>
+            <UIButton class="btn" @click="resize('xlarge')" >超大</UIButton>
+        </div>
+        <div class="btn-group">
+            <UIButton class="btn" @click="changeRadius('tile')">矩形</UIButton>
+            <UIButton class="btn" @click="changeRadius('normal')">正常</UIButton>
+            <UIButton class="btn" @click="changeRadius('rounded')" >半圆</UIButton>
+            <UIButton class="btn" @click="changeRadius('circle')" >圆形</UIButton>
+        </div>
+        <div class="btn-group">
+            <UIButton class="btn" @click="changeDisable">禁用</UIButton>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Vue, Component} from 'vue-property-decorator'
+    import UIButton from "@/components/UIKIT/UIButton.vue";
+
+    @Component({
+        components: {
+            UIButton
+        }
+    })
+    export default class Home extends Vue {
+        /* eslint-disable  */
+        private xsmall: boolean = false;
+        private small: boolean = false;
+        private large: boolean = false;
+        private xlarge: boolean = false;
+        private tile: boolean = false;
+        private rounded: boolean = false;
+        private circle: boolean = false;
+        private disabled: boolean = false;
+
+        private clickBtn(event: MouseEvent) {
+            console.log(event);
+        }
+
+        private resize(name: string) {
+            switch (name) {
+                case 'xsmall':
+                    this.xsmall = true
+                    this.small = false
+                    this.large = false
+                    this.xlarge = false
+                    break
+                case 'small':
+                    this.xsmall = false
+                    this.small = true
+                    this.large = false
+                    this.xlarge = false
+                    break
+                case 'normal':
+                    this.xsmall = false
+                    this.small = false
+                    this.large = false
+                    this.xlarge = false
+                    break
+                case 'large':
+                    this.xsmall = false
+                    this.small = false
+                    this.large = true
+                    this.xlarge = false
+                    break
+                case 'xlarge':
+                    this.xsmall = false
+                    this.small = false
+                    this.large = false
+                    this.xlarge = true
+                    break
+            }
+        }
+        private changeRadius(name: string) {
+            switch (name) {
+                case 'tile':
+                    this.tile = true
+                    this.rounded = false
+                    this.circle = false
+                    break
+                case 'normal':
+                    this.tile = false
+                    this.rounded = false
+                    this.circle = false
+                    break
+                case 'rounded':
+                    this.tile = false
+                    this.rounded = true
+                    this.circle = false
+                    break
+                case 'circle':
+                    this.tile = false
+                    this.rounded = false
+                    this.circle = true
+                    break
+            }
+        }
+        private changeDisable(){
+            this.disabled = !this.disabled
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+    .btn-container
+        width 480px
+        height 160px
+        /*background-color red*/
+        display flex
+        justify-content center
+        align-items center
+
+    .btn
+        margin 6px
+</style>
